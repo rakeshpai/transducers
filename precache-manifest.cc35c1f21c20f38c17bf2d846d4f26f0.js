3a7cfeaa578d40412b03c63bdb97b503b4577dab
Rakesh Pai <rakeshpai@gmail.com>
2019-07-20 20:53:42 +0530

    Updates

---
+++ precache-manifest.cc35c1f21c20f38c17bf2d846d4f26f0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "28d3ee07131f861f1101",
+    "url": "/transducers/static/js/main.28d3ee07.chunk.js"
+  },
+  {
+    "revision": "8fe8099abe9ac5af64ea",
+    "url": "/transducers/static/css/1.250cc90b.chunk.css"
+  },
+  {
+    "revision": "8fe8099abe9ac5af64ea",
+    "url": "/transducers/static/js/1.8fe8099a.chunk.js"
+  },
+  {
+    "revision": "782736e3ccfab2425ce9",
+    "url": "/transducers/static/js/runtime~main.782736e3.js"
+  },
+  {
+    "revision": "316e299e711e1b475b036d542cda37bd",
+    "url": "/transducers/static/media/slide-background.316e299e.webp"
+  },
+  {
+    "revision": "92014aaf8a503a7ddaf3362e93f6d792",
+    "url": "/transducers/static/media/paper.92014aaf.webp"
+  },
+  {
+    "revision": "8d4869febac27a5e0f3c8fe7bd41f6c0",
+    "url": "/transducers/static/media/pure.8d4869fe.webp"
+  },
+  {
+    "revision": "28e99dd369d8046ca894269e4c253288",
+    "url": "/transducers/static/media/astronomy.28e99dd3.webp"
+  },
+  {
+    "revision": "15cc1a01f615b1a7d0515276cf7d1d13",
+    "url": "/transducers/static/media/curry.15cc1a01.webp"
+  },
+  {
+    "revision": "d2577d948e63acd2fee0711c2629d8d3",
+    "url": "/transducers/static/media/composition.d2577d94.webp"
+  },
+  {
+    "revision": "64c3ce22cc7b63ce569d667678376819",
+    "url": "/transducers/static/media/super-power.64c3ce22.webp"
+  },
+  {
+    "revision": "b27d298ef6b2c03164c65427cd828fbf",
+    "url": "/transducers/static/media/fork.b27d298e.webp"
+  },
+  {
+    "revision": "7965f68692acfcfe93d5ed3989d4e7e5",
+    "url": "/transducers/static/media/no-compromise.7965f686.webp"
+  },
+  {
+    "revision": "c6ea2c98356dd012b5f7721c52541984",
+    "url": "/transducers/index.html"
+  }
+];
